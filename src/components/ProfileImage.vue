9625436ce529c532d9b4d6817ca8d130a9a6e6e2
Fatih Mehmet Baydar <baydarmf@gmail.com>
2023-01-27 21:05:23 +0300

    ProfileImage created

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,17 @@
+<template>
+  <img
+    :src="profileImage"
+    alt="Profile Photo"
+    class="h-8 w-8 rounded-3xl object-contain"
+  />
+</template>
+
+<script>
+export default {
+  name: "ProfileImage",
+  data: () => ({
+    profileImage:
+      "https://www.pngitem.com/pimgs/b/156-1569018_jack-sparrow-png.png",
+  }),
+};
+</script>
